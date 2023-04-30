c900bd566d162dd0adbe22028560f7fb95a787f1
nmkhans <nurmoin05@gmail.com>
2023-05-01 00:45:12 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
