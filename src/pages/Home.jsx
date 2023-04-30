5e1d398657c5c91623fa134d5babb895ea66e1b2
nmkhans <nurmoin05@gmail.com>
2023-05-01 03:43:10 +0600

    slide added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,89 @@
-import Layout from "../components/Layout";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper";
+
+// Import Swiper styles
+import "swiper/css/bundle";
+
+import Images from "./../util/Images";
 
 const Home = () => {
   return (
-    <Layout>
-      Home
-    </Layout>
+    <section className="h-[500px]">
+      <Swiper
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        autoplay={{ delay: 3000 }}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="h-[550px] w-full object-cover"
+              src={Images.slideOne}
+              alt=""
+            />
+            <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full z-10"></div>
+            <div className="absolute top-[50%] left-[10%] w-full flex justify-start items-center z-20 text-white">
+              hello
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="h-[550px] w-full object-cover"
+              src={Images.slideTwo}
+              alt=""
+            />
+            <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full z-10"></div>
+            <div className="absolute top-[50%] left-[10%] w-full flex justify-start items-center z-20 text-white">
+              hello
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="h-[550px] w-full object-cover"
+              src={Images.slideThree}
+              alt=""
+            />
+            <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full z-10"></div>
+            <div className="absolute top-[50%] left-[10%] w-full flex justify-start items-center z-20 text-white">
+              hello
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="h-[550px] w-full object-cover"
+              src={Images.slideFour}
+              alt=""
+            />
+            <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full z-10"></div>
+            <div className="absolute top-[50%] left-[10%] w-full flex justify-start items-center z-20 text-white">
+              hello
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="h-[550px] w-full object-cover"
+              src={Images.slideFive}
+              alt=""
+            />
+            <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full z-10"></div>
+            <div className="absolute top-[50%] left-[10%] w-full flex justify-start items-center z-20 text-white">
+              hello
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
--- src/util/Images.js
+++ src/util/Images.js
@@ -1,9 +1,19 @@
 import homeImg from "@/assets/home.jpg";
 import logo from "@/assets/logo.svg";
+import slideOne from "@/assets/slide_1.jpg";
+import slideTwo from "@/assets/slide_2.jpg";
+import slideThree from "@/assets/slide_3.jpg";
+import slideFour from "@/assets/slide_4.jpg";
+import slideFive from "@/assets/slide_5.jpg";
 
 const Images = {
   homeImg,
   logo,
+  slideOne,
+  slideTwo,
+  slideThree,
+  slideFour,
+  slideFive,
 };
 
 export default Images;
